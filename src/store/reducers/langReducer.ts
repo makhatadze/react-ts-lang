c53de6627a3765dd0797d6f87cb60551fe9effaa
makhatadze <vitomaxatadze@gmail.com>
2021-04-22 16:39:12 +0400

    Make langReducer function, which return new state on event actions.

--- src/store/reducers/langReducer.ts
+++ src/store/reducers/langReducer.ts
@@ -1,7 +1,21 @@
-import {SET_LANGUAGE, LangState, LangAction} from "../types";
+import {LangAction, LangState, SET_LANGUAGE} from "../types";
 
 const localStoragelang = localStorage.getItem('language');
 
 const initialState: LangState = {
     language: localStoragelang? localStoragelang : 'en'
 }
+
+const langReducer = (state = initialState, action: LangAction) => {
+    switch (action.type){
+        case SET_LANGUAGE:
+            return{
+                ...state,
+                language: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default langReducer;
